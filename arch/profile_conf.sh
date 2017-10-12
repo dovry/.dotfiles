2ae0814f09e92492ca1645092c7e2057d2211efc
Dovry <15201298+Dovry@users.noreply.github.com>
2017-10-12 21:12:49 +0200

    Update profile_conf.sh

--- arch/profile_conf.sh
+++ arch/profile_conf.sh
@@ -4,7 +4,7 @@ PROFILE=/etc/profile.d
 
 sudo touch $PROFILE/{wallpaper,mute_speaker}.sh
 
-sudo echo "nitrogen --restore &" >> $PROFILE/prowallpaper.sh
+sudo echo "nitrogen --restore &" >> $PROFILE/wallpaper.sh
 sudo echo "xset b off" >> $PROFILE/mute_speaker.sh
 
 echo "all done :)"
