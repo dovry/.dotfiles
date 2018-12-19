6950649cb5603d48d92fc10c916350c6c7c15b13
Dovry <einzyd@gmail.com>
2018-12-19 10:48:07 +0100

    total rewrite
    actually set up an ubuntu VM with the script and it actually works

--- fresh-install/fresh-install-WIP.sh
+++ fresh-install/fresh-install-WIP.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 set -e
 
-# gets my .bash_aliases file and puts it in the home directory
+# gets .bash_aliases and puts it in the home directory
+# This uses wget because curl is not installed by default on Ubuntu desktops
 wget https://raw.githubusercontent.com/Dovry/dotfiles/master/.bash_aliases -P ~/.
 # sets the ps1 config and reloads the .bashrc for everything to take effect
 crazyvariablenobodycouldeveruse=$(wget https://raw.githubusercontent.com/Dovry/dotfiles/master/fresh-install/ps1 -q -O -)
 echo $crazyvariablenobodycouldeveruse >> ~/.bashrc
-source ~/.bashrc
 
-# requires the .bash_aliases file and runs a full update & upgrade on the system
-upd && upg
+# runs a full update & upgrade on the system
+sudo apt update -y
+sudo apt upgrade
+
 # starts installing packages
-install -y avahi-daemon tmux vim \
-	 rxvt-unicode tree htop git \
-	 curl
+sudo apt install -y avahi-daemon tmux vim \
+tree htop git curl
 
-# removes junk
-uninstall -y --purge libreoffice-core aisleriot* gnome-mines* gnome-sudoku
-uninstall -y ubuntu-web-launchers rhythmbox cheese 
+# removes junk, most of this is not installed with the minimal version
+sudo apt remove -y --purge libreoffice* aisleriot* gnome-mines* gnome-sudoku
+sudo apt remove -y ubuntu-web-launchers rhythmbox cheese 
 
-#autoremove
-aa -y
+# clean up unused package dependencies
+sudo apt autoremove -y
 
-# fetches config files for tmux & vim
-newvim && newtmux
+# fetches config files for tmux
+wget https://raw.githubusercontent.com/Dovry/dotfiles/master/.tmux.conf -P ~/
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+tmux source-file ~/.tmux.conf && ~/.tmux/plugins/tpm/bin/install_plugins
+clear && echo "tmux config active"
 
-# fetches vim colorschemes, puts them in the correct folder, then removes the theme file
-# This is required by ~/.vimrc
-# requires package 'git' to work
-mkdir ~/.vim/colors
-wget -P ~/. https://raw.githubusercontent.com/Dovry/dotfiles/master/fresh-install/vim-themes.txt
-wget -P ~/.vim/colors/ -i vim-themes.txt
-rm ~/.vim-themes.txt
+# fetches Vim config
+wget https://raw.githubusercontent.com/Dovry/dotfiles/master/fresh-install/config-install/vim-install.sh -P ~/
+chmod +x ~/vim-install.sh && sh ~/vim-install.sh && rm ~/vim-install.sh
 
+# Clean up the homefolder
+rmdir ~/{Music,Public,Templates,Videos,Desktop}
+mkdir .backups
 
 # release the variables back into the wild
 unset crazyvariablenobodycouldeveruse #ironic
+
+# tells user to source .bashrc
+echo "Source your .bashrc with 'source .bashrc' to complete the setup"
