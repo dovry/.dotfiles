256427cfd5e7dafa39e97742b1f765229608b07d
Dovry <einzyd@gmail.com>
2019-02-28 09:49:12 +0100

    Update root-check.sh

--- root-check.sh
+++ root-check.sh
@@ -7,11 +7,10 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 # echo "${red}red text ${green}green text${reset}"
 
-# use != if it must be run as root
+# use != and remove 'NOT' if it must be run as root
 if [ "$(whoami)" == 'root' ]; then
   printf "${green}~~~\n${red}This script must NOT be run as root\n${green}~~~${reset}\n"
   exit 1
 fi
 
-user=$(whoami)
-echo $user
+echo $(whoami)
