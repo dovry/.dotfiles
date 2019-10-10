fb3a389bcfe63f83e61363a6d1b6a5e1b58bd20e
Dovry <beepboopvm@gmail.com>
2019-10-10 08:09:00 +0200

    removed arch

--- gnome-shortcuts/vs-code.sh
+++ gnome-shortcuts/vs-code.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-notify-send "Launching VS Code" "One moment"
+#!/bin/bash
+scriptDir=$(dirname -- "$(readlink -f -- "${BASH_SOURCE[0]}")")
+notify-send -i "$scriptDIR/icons/vs-code.png" "Launching VS Code" "One moment"
 code
