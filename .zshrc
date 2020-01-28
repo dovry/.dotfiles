667a62f1601abd4dabe30a47cb30d818b91f7b00
Dovry <beepboopvm@gmail.com>
2020-01-28 14:16:11 +0100

    ready for merge to master

--- .zshrc
+++ .zshrc
@@ -1,55 +1,6 @@
-# 2.0.0 - zshrc
-# X.0.0 major 	- the file is overhauled
-# 0.X.0 minor		- commands are added or removed
-# 0.0.X fix 		- the file is improved in any other way
+# your original file was backed up to ~/.zshrc.orig
 
-# Path to your oh-my-zsh installation.
-export ZSH=$HOME/.oh-my-zsh
 
-# zshrc location and source the dotfiles
-#Z_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-Z_LOC="$(dirname "$(readlink -f "$0")")"
-source "$Z_LOC"/.shell_vars
-source "$Z_LOC"/.shell_aliases
-source "$Z_LOC"/.shell_functions
+### MANAGED BY DOTFILES
 
-ZSH_THEME="wezm"
-HYPHEN_INSENSITIVE="true"
-COMPLETION_WAITING_DOTS="true"
-DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-plugins=(
-  ansible
-  autojump
-  command-not-found
-  copyfile
-  dircycle
-  dirhistory
-  docker
-  git
-  git-extras
-  perms
-  sudo
-  tmux
-  vagrant
-  vscode
-)
-
-# Preferred editor for local and remote sessions
-if [[ -n $SSH_CONNECTION ]]; then
-  export EDITOR='vim'
-fi
-
-if [ -f "$HOME/.config/.qfc/bin/qfc.sh" ]; then
-# git clone https://github.com/pindexis/qfc $HOME/.config/.qfc
-  [[ -s "$HOME/.config/.qfc/bin/qfc.sh" ]] && source "$HOME/.config/.qfc/bin/qfc.sh"
-  qfc_quick_command 'cd' '\C-b' 'cd $0'
-  qfc_quick_command 'vim' '\C-p' 'vim $0'
-fi
-
-# Export functions
-for i in $(grep -Po "\w+ \(\)" ~/.dotfiles/.shell_functions | awk '{ print $1 }'); do
- export -f $i
-done
-
-source $ZSH/oh-my-zsh.sh
+### END MANAGED BY DOTFILES
--- shell_setup.sh
+++ shell_setup.sh
@@ -3,25 +3,35 @@
 . .shell_vars
 
 # Check which shell, then run config
-if [ "$CURRENT_SHELL" = bash ]; then
-# For bash - edit ~/.bashrc to source the aliases
-  if grep -qo "$LEAD" "$BASHRC"; then
+if [[ "$CURRENT_SHELL" = zsh || "$CURRENT_SHELL" = bash ]]; then
+  if grep -qo "$LEAD" "$RC"; then
     :
+  else
+    if [[ -f ~/.zshrc.orig ]]; then
+      :
     else
-    printf "\n%s\n\n%s" "$LEAD" "$TAIL" >> "$BASHRC"
+      cp ~/.zshrc ~/.zshrc.orig
+      printf "# your original file was backed up to ~/.zshrc.orig" > ~/.zshrc
+    fi
+    printf "\n\n\n%s\n\n%s" "$LEAD" "$TAIL" >> "$RC"
+  fi
+
+# clear ZSH_SOURCE var if zsh is not the shell
+  if [[ "$CURRENT_SHELL" == bash ]]; then
+    ZSH_SOURCE=
   fi
 
   # Make folder colors readable on WSL
-  if grep -qP "WSL" ~/.bashrc || grep -qP "(Microsoft|WSL)" /proc/version; then
+  if grep -qP "WSL" "$RC" || grep -qP "(Microsoft|WSL)" /proc/version; then
     WSL_DIR_COL="  LS_COLORS=\"ow=01;36;40\" && export LS_COLORS #WSL dir colors\n"
-    else
+  else
     WSL_DIR_COL=""
   fi
 
-  if [ ! "$(which autojump)" ]; then
-    AUTOJUMP=""
-    else
+  if which autojump; then
     AUTOJUMP=". /usr/share/autojump/autojump.sh"
+    else
+    AUTOJUMP=
   fi
 
   if [ -f "$HOME/.qfc/README.md" ]; then
@@ -41,11 +51,13 @@ $DOTFILES\
 \n\
 $PS1\
 \n\
-$TAIL" "$BASHRC"
+$ZSH_RC\
+\n\
+$ZSH_SOURCE\
+\n\n\
+$TAIL" "$RC"
 
-elif [ "$CURRENT_SHELL" = zsh ]; then
-  ln -sf "$DOT_LOC/.zshrc" "$ZSHRC"
-elif [ ! "$CURRENT_SHELL" = zsh ] || [ ! "$CURRENT_SHELL" = bash ]; then
+else
   printf "\nCurrent shell is %s\nAliases and functions only work with %s\n\n" "$CURRENT_SHELL" "$SUPPORTED_SHELLS"
 fi
 
@@ -54,6 +66,6 @@ for file in "$DOT_LOC"/.{vimrc,tmux.conf}; do
 ln -sf "$file" "/home/$USERNAME/"
 done
 
-if [ ! "$(which git)" ] || [ ! "$(which curl)" ]; then 
+if [[ ! "$(which git)" || ! "$(which curl)" ]]; then 
 printf "\n\nvim requires git and curl to install plugins!\n\n"
 fi
